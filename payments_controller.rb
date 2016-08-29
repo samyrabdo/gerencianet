e36f9081ef06f904d0ef135266c1316759c7d1c8
samyrabdo <samyr.info@gmail.com>
2016-08-29 07:36:51 -0300

    Add files via upload

---
+++ payments_controller.rb
@@ -0,0 +1,61 @@
+
+class PaymentsController < ApplicationController
+
+# POST /payments
+# POST /payments.json
+def create
+
+require "gerencianet"
+require "date"
+ tomorrow = Date.today + 1
+
+
+options = {
+client_id: "minhaChave",
+client_secret: "minhaChave",
+  sandbox: true
+}
+ 
+charge = {
+  items: [{
+    name: "Product 1",
+    value: 1000,
+    amount: 2
+  }],
+  shippings: [{
+    name: "Default Shipping Cost",
+    value: 100
+  }, {
+    name: "Adicional Shipping Cost",
+    value: 150
+  }]
+}
+ 
+payment = {
+  payment: {
+     banking_billet: {
+      expire_at: tomorrow.strftime,
+      customer: {
+        name: "Gorbadoc Oldbuck",
+        email: "oldbuck@gerencianet.com.br",
+        cpf: "94271564656",
+        birth: "1977-01-15",
+        phone_number: "5144916523"
+      }
+    }
+  }
+}
+ 
+gerencianet = Gerencianet.new(options)
+ 
+charge = gerencianet.create_charge(body: charge)
+ 
+params = {
+  id: charge["data"]["charge_id"]
+}
+ 
+puts gerencianet.pay_charge(params: params, body: payment)
+
+end
+
+end
